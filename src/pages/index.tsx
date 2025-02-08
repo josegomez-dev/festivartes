14942b99494aa7efa0878ace180cf42d82dbedda
Jose Alejandro Gomez Castro <josealejandrogomezcastro@Joses-MacBook-Pro.local>
2025-02-08 13:57:33 -0600

    update main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,120 +1,34 @@
 import styles from "@/app/assets/styles/MainPage.module.css";
+import { time } from "console";
 import Image from "next/image";
 import Link from "next/link";
-// import { FaPlus } from 'react-icons/fa';
+import { useEffect } from "react";
 
 const Home = () => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = '/login';
+    }, 1000);
+  }, []);
+
   return (
     <main className={styles.main}>
-      <br />
-      <br />
-      <br />
-      <br />
-      <div className={styles.description}>
-        <p>
-            <Link href="/login" className="project-miniature-link">
-              Iniciar sesión 
-            </Link>
-            |
-            <Link href="/signup" className="project-miniature-link"> 
-              Registrarse 
-            </Link>
-        </p>
-        <div>
-          <a
-            href="https://app.daily.dev/josegomezdev"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="project-miniature-link"
-          >
-            By{" "}
-            <b>José Alejandro Gómez Castro</b>
-          </a>
-        </div>
-      </div>
-
       <div className={styles.center}>
         <div className="logo-positioning-for-mobile">
-          <Image
-            className={styles.logo}
-            src="/logo2.png"
-            alt="Festivartes Main Logo"
-            width={400}
-            height={400}
-            priority
-          />
+          <h1>Bienvenido a <b style={{ fontSize: '1.5em'}}> FESTIVARTES</b></h1>
+          <Link href="/login">
+              <Image
+              className={styles.logo}
+              src="/logo2.png"
+              alt="Festivartes Main Logo"
+              width={400}
+              height={400}
+              priority
+            />
+          </Link>
         </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="#"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Festivartes:
-          </h2>
-          <p>
-          Es una plataforma con <b>servicio premium</b> para quienes desean una experiencia artística sin
-límites. Ofrece suscripciones adaptadas a diferentes tipos de usuarios: <b>personal, empresas
-y entidades gubernamentales</b>.
-          </p>
-        </a>
-
-        <a
-          href="#"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Servicio Premium:
-          </h2>
-          <p>
-          Con características
-avanzadas y acceso exclusivo a eventos especiales y talleres, <b>Festivartes</b> está diseñado
-para aquellos que buscan conectar profundamente con <b>el arte y la cultura</b>, aprovechando
-beneficios exclusivos.
-          </p>
-        </a>
-
-        <a
-          href="#"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Roles:
-          </h2>
-          <p>
-            <b>Administración:</b> Usuarios encargados de la gestión de la plataforma
-          y sus funciones. <br />
-            <b>Jurado Seleccionador:</b> Participantes que califican y puntúan las obras presentadas
-          en eventos. <br />
-            <b>Usuario General:</b> Subcategoría para Artistas, Compositores y Creadores de Arte en general.
-
-          </p>
-        </a>
-
-        <a
-          href="#"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-          Obras Musicales: <b>Premiación y Evaluación</b>
-          </h2>
-          <p>
-          <b>(Calificaciones y Votaciones)</b> &nbsp;Sistema de recompensas para participantes destacados: Mecanismo para
-que el jurado y el público evalúen las presentaciones.
-          </p>
-        </a>
-      </div>
-      <div className="custom-nav-spacer" />
     </main>
   )
 }
